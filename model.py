be7f6db3e425de4b675c46907d7b7dfc06112a41
CurryChen <chenkeyu7777@gmail.com>
2023-05-15 11:22:06 +0800

    rgb_cam state and discrete action

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import random
 import time
 import numpy as np
 import carla
+import cv2
 from collections import deque
 import torch.nn.functional as F
 from torch import nn
@@ -16,10 +17,16 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class CarEnv:
-    def __init__(self):
+    def __init__(self,
+                 im_width,
+                 im_height,
+                 throttle_list,
+                 steer_list,
+                 desired_speed=40,
+                 show_cam=True):
         # Create a Carla client and connect to the Carla server
         self.client = carla.Client("localhost", 2000)
-        self.client.set_timeout(10.0)
+        self.client.set_timeout(5.0)
 
         # Get the Carla world and blueprint library
         self.world = self.client.get_world()
@@ -33,87 +40,125 @@ class CarEnv:
         # Get the vehicle blueprint and spawn the vehicle
         self.model3 = self.blueprint_library.find("vehicle.tesla.model3")
 
+        # desired speed
+        self.desired_speed = desired_speed
+
+        # image width height
+        self.im_width = im_width
+        self.im_height = im_height
+        self.show_cam = show_cam
+        self.front_camera = None
+
+        self.throttle_list = throttle_list
+        self.steer_list = steer_list
+
     def reset(self):
         self.actor_list = []
         self.sensor_list = []
         self.collision_hist = []
+        self.cross_lane_hist = []
         # Reset the vehicle to a random position and velocity
         car_transform = random.choice(self.world_map.get_spawn_points())
-        self.vehicle = self.world.spawn_actor(self.model3, car_transform)
+        try:
+            self.vehicle = self.world.spawn_actor(self.model3, car_transform)
+        except RuntimeError:
+            print("first spawn failed, create a new spawn points")
+            car_transform_new = random.choice(self.world_map.get_spawn_points())
+            self.vehicle = self.world.spawn_actor(self.model3, car_transform_new)
         self.vehicle.set_simulate_physics(True)
         self.actor_list.append(self.vehicle)
 
-        # create a list to detect collision
+        # when init a car, first pause 2s
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
+        time.sleep(2)
+
+        # create a sensor to get the rgb image
+        self.rgb_cam = self.world.get_blueprint_library().find('sensor.camera.rgb')
+        self.rgb_cam.set_attribute('image_size_x', f'{self.im_width}')
+        self.rgb_cam.set_attribute('image_size_y', f'{self.im_height}')
+        self.rgb_cam.set_attribute('fov', '110')
+
+        cam_transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        self.camera = self.world.spawn_actor(self.rgb_cam, cam_transform, attach_to=self.vehicle)
+        self.sensor_list.append(self.camera)
+        self.camera.listen(lambda data: self.process_img(data))
+
+        # create a sensor to detect collision
         colsensor = self.blueprint_library.find("sensor.other.collision")
-        sensor_transform = carla.Transform(carla.Location(x=2.5, z=0.7))
-        self.colsensor = self.world.spawn_actor(colsensor, sensor_transform, attach_to=self.vehicle)
+        colsensor_transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+        self.colsensor = self.world.spawn_actor(colsensor, colsensor_transform, attach_to=self.vehicle)
         self.sensor_list.append(self.colsensor)
         self.colsensor.listen(lambda event: self.collision_data(event))
 
-        # Get the vehicle's state information
-        state = self.get_state()
-        return state
+        # create a sensor to detect crossing lane
+        cross_lane_sensor = self.blueprint_library.find("sensor.other.lane_invasion")
+        cro_sensor_transform = carla.Transform(carla.Location(x=2.5, z=0.5))
+        self.cross_lane_sensor = self.world.spawn_actor(cross_lane_sensor, cro_sensor_transform, attach_to=self.vehicle)
+        self.sensor_list.append(self.cross_lane_sensor)
+        self.cross_lane_sensor.listen(lambda event: self.cross_lane_data(event))
+
+        while self.front_camera is None:
+            time.sleep(0.01)
+        # self.front_camera [H ,W, 3]
+        return self.front_camera
 
     def collision_data(self, event):
         self.collision_hist.append(event)
 
-    def get_state(self):
-        # Get the vehicle's location, velocity, and orientation
-        location = self.vehicle.get_location()
-        velocity = self.vehicle.get_velocity()
-        orientation = self.vehicle.get_transform().rotation.yaw
-        # Return a numpy array of the state information
-        state = np.array([location.x, location.y, velocity.x])
-        return state
+    def cross_lane_data(self, event):
+        self.cross_lane_hist.append(event)
 
-    def step(self, action):
-        control = carla.VehicleControl(throttle=0.8, steer=float(action-1)/2)
+    def process_img(self, image):
+        raw_image = np.array(image.raw_data)
+        image_4channel = raw_image.reshape((self.im_height, self.im_width, 4))
+        rgb_image = image_4channel[:, :, :3]
+        if self.show_cam:
+            cv2.imshow("front_image", rgb_image)
+            cv2.waitKey(1)
+        # rgb_image numpy form [H, W, 3]
+        self.front_camera = rgb_image
+
+    def step(self, action, steer_dim):
+        throttle_action = torch.div(action, steer_dim, rounding_mode="floor")  # The number of times you can go into it
+        steer_action = action % steer_dim  # the remainder
+        control = carla.VehicleControl(throttle=self.throttle_list[throttle_action], steer=self.steer_list[steer_action])
         self.vehicle.apply_control(control)
-        time.sleep(0.05)
-        location = self.vehicle.get_location()
+        # time.sleep(0.05)
+
         v = self.vehicle.get_velocity()
         kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
 
+        # collision flag
         if len(self.collision_hist) != 0:
             done = True
-            reward = -200
-        elif kmh < 50:
-            done = False
-            reward = -1
+            col_flag = 1
         else:
+            col_flag = 0
             done = False
-            reward = 1
 
-        return (location.x, location.y, kmh), reward, done
+        # over speed flag
+        if kmh > self.desired_speed:
+            overspeed_flag = 1
+        else:
+            overspeed_flag = 0
 
+        # out of the lane
+        out_of_lane_flag = len(self.cross_lane_hist)
 
-# class DQN(nn.Module):
-#     def __init__(self):
-#         super(DQN, self).__init__()
-#         # load the model
-#         model = models.resnet50(weights=False)
-#         # get the input fc features
-#         fc_features = model.fc.in_features
-#         # modify the output class of the prediction head to 3(right, left, straight)
-#         model.fc = nn.Linear(fc_features, 3)
-#         # add the softmax
-#         model.fc = nn.Sequential(model.fc, nn.Softmax(dim=1))
-#         self.model = model
-#
-#     def forward(self, x):
-#         x = self.model(x)
+        reward = -300 * col_flag + 0.1 * kmh + -1 * overspeed_flag + -1 * out_of_lane_flag
+        # self.front_camera: numpy [H, W, 3]
+        return self.front_camera, reward, done
+
 
 class DQN(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, input_channel, action_dim):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 64)
-        self.fc2 = nn.Linear(64, 128)
-        self.fc3 = nn.Linear(128, action_dim)
+        self.input_channel = input_channel
+        self.action_dim = action_dim
+        self.mobilnet = models.mobilenet_v3_small(num_classes=self.action_dim)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.mobilnet(x)
         return x
 
 
@@ -121,6 +166,7 @@ class ReplayBuffer:
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
 
+    # state, next_state: numpy [H, W, 3]
     def push(self, state, action, reward, next_state, done):
         self.buffer.append((state, action, reward, next_state, done))
 
@@ -133,12 +179,29 @@ class ReplayBuffer:
 
 
 class DQNAgent:
-    def __init__(self, state_dim, action_dim, gamma=0.99, epsilon=1.0, epsilon_decay=0.9999, epsilon_min=0.01, lr=1e-3, buffer_capacity=10000, batch_size=64, update_interval=10):
+    def __init__(self,
+                 im_width,
+                 im_height,
+                 throttle_list,
+                 steer_list,
+                 input_channel=3,
+                 show_cam=True,
+                 pretrain_path=None,
+                 gamma=0.99,
+                 epsilon=1.0,
+                 epsilon_decay=0.9999,
+                 epsilon_min=0.01,
+                 lr=1e-3,
+                 buffer_capacity=10000,
+                 batch_size=8,
+                 update_interval=10):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.state_dim = state_dim
-        self.action_dim = action_dim
+        self.input_channel = input_channel
+        self.im_width = im_width
+        self.im_height = im_height
+        self.show_cam = show_cam
+        self.pretrain_path = pretrain_path
         self.gamma = gamma
-        self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.lr = lr
@@ -146,42 +209,110 @@ class DQNAgent:
         self.batch_size = batch_size
         self.update_interval = update_interval
         self.memory = ReplayBuffer(buffer_capacity)
-        self.q_network = DQN(self.state_dim, self.action_dim).to(self.device)
-        self.target_network = DQN(self.state_dim, self.action_dim).to(self.device)
+        self.epsilon = epsilon
+        # create possible action list
+        self.throttle_list = throttle_list
+        self.steer_list = steer_list
+        self.throttle_dim = len(self.throttle_list)
+        self.steer_dim = len(self.steer_list)
+        self.action_dim = self.throttle_dim + self.steer_dim  # 根据throttle和steer计算动作空间大小
+
+        # init the network
+        self.q_network = DQN(self.input_channel, self.action_dim).to(self.device)
+        self.target_network = DQN(self.input_channel, self.action_dim).to(self.device)
         self.target_network.load_state_dict(self.q_network.state_dict())
+        self.target_network.eval()
         self.optimizer = optim.Adam(self.q_network.parameters(), lr=self.lr)
-        self.loss_function = nn.MSELoss()
+        self.loss_function = nn.SmoothL1Loss()
+
+        # load the pre-trained model parameters
+        if self.pretrain_path:
+            self.load_model(self.pretrain_path)
+
         # create the carla environment
-        self.env = CarEnv()
+        self.env = CarEnv(im_width=self.im_width, im_height=self.im_height,
+                          throttle_list=self.throttle_list, steer_list=self.steer_list, show_cam=self.show_cam)
 
     def act(self, state):
-        # state: (location.x, location.y, kmh)
+        # state: tensor [1, 3, H, W]
         self.epsilon = max(self.epsilon*self.epsilon_decay, self.epsilon_min)
         if random.random() > self.epsilon:
             with torch.no_grad():
-                state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+                # state: tensor [1, 3, H, W]
+                state = state.to(self.device)
                 q_values = self.q_network(state)
-                action = q_values.argmax().item()
+                # the front self.throttle_dim of q_values belong to throttle
+                throttle_q_values = q_values[:self.throttle_dim]
+                # find the max index of throttle
+                throttle = torch.argmax(throttle_q_values).item()
+                # the rest q_values belong to the throttle
+                steer_q_values = q_values[self.throttle_dim:]
+                # find the max index of steer
+                steer = torch.argmax(steer_q_values).item()
         else:
-            action = random.randrange(self.action_dim)
-        # action: 0, 1, 2
-        return action
+            # random choose the index of throttle and steer
+            throttle = random.randrange(self.throttle_dim)
+            steer = random.randrange(self.steer_dim)
 
-    def update_network(self, i):
+        # clever way to save the throttle index and steer index using one index
+        # multiply self.steer_dim because steer will never bigger than self.steer_dim
+        return throttle * self.steer_dim + steer
+
+    def update_network(self):
+        # state tuple([H, W, 3], [H, W, 3]........)
         state, action, reward, next_state, done = self.memory.sample(self.batch_size)
-        state = torch.FloatTensor(state).to(self.device)
+        state = torch.FloatTensor(np.array(state)).permute(0, 3, 1, 2).to(self.device)
+        # state [batch_size, 3, H, W]
         action = torch.LongTensor(action).unsqueeze(1).to(self.device)
+        # action [batch_size, 1]
         reward = torch.FloatTensor(reward).unsqueeze(1).to(self.device)
-        next_state = torch.FloatTensor(next_state).to(self.device)
+        next_state = torch.FloatTensor(np.array(next_state)).permute(0, 3, 1, 2).to(self.device)
+        # next_state [batch_size, 3, H, W]
         done = torch.FloatTensor(done).unsqueeze(1).to(self.device)
 
-        q_values = self.q_network(state).gather(1, action)
-        target_q_values = self.target_network(next_state).max(1)[0].unsqueeze(1)
-        target_q_values = reward + self.gamma * target_q_values * (1 - done)
+        # transform the unify index of throttle and steer to their individual one
+        throttle_action = torch.div(action, self.steer_dim, rounding_mode="floor")  # The action index of throttle
+        steer_action = action % self.steer_dim  # the remainder
 
-        loss = F.mse_loss(q_values, target_q_values.detach())
+        # seperate the unify q_values to their individual one using their corresponding index
+        q_values = self.q_network(state)
+        throttle_q_values = q_values[:, :self.throttle_dim]
+        steer_q_values = q_values[:, self.throttle_dim:]
+
+        # normalize the throttle_q_values
+        throttle_q_values = F.softmax(throttle_q_values, dim=-1)
+        throttle_q_values = throttle_q_values.gather(1, throttle_action).squeeze(1)
+        steer_q_values = steer_q_values.gather(1, steer_action).squeeze(1)
+
+        # current_q_values [batch_size]
+        current_q_values = throttle_q_values + steer_q_values
+
+        # compute the target q values
+        next_q_values = self.target_network(next_state)
+        next_throttle_q_values = next_q_values[:, :self.throttle_dim]
+        next_steer_q_values = next_q_values[:, self.throttle_dim:]
+        next_throttle_actions = next_throttle_q_values.max(-1)[1]
+        next_steer_actions = next_steer_q_values.max(-1)[1]
+        next_q_values = next_throttle_q_values.gather(1, next_throttle_actions.unsqueeze(1)).squeeze(0) + \
+                        next_steer_q_values.gather(1, next_steer_actions.unsqueeze(1)).squeeze(0)
+
+        # target_q_values [batch_size]
+        target_q_values = (reward + self.gamma * next_q_values * (1 - done)).squeeze(1)
+
+        # compute the loss of the q_network
+        loss = F.mse_loss(current_q_values, target_q_values.detach())
         # only update the parameter of the q network
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
         return loss
+
+    def load_model(self, pretrain_path):
+        # only load the parameter, need to have a initial model structrue
+        pretrain_model = torch.load(pretrain_path)
+        print("successfully load the pretrained model")
+        self.q_network.load_state_dict(pretrain_model["model_dict"])
+        self.target_network.load_state_dict(self.q_network.state_dict())
+        self.optimizer.load_state_dict(pretrain_model['optimizer_dict'])
+        self.pretrain_episode = pretrain_model["episode"]
+        self.epsilon = pretrain_model['epsilon']
--- train.py
+++ train.py
@@ -2,79 +2,139 @@
 # written by chenkeyu
 
 import math
+import os
 import random
 import time
 import numpy as np
 import carla
 import torch
 from torch.utils.tensorboard import SummaryWriter
-from Carla_RL.model import DQNAgent
+from model import DQNAgent
 
 
 class AgentTrainer:
-    def __init__(self, agent, episode):
+    def __init__(self,
+                 agent,
+                 episodes,
+                 save_episodes=100,
+                 target_update_episodes=10,
+                 model_name="MobileNet_v3",
+                 save_path="pretrained_models/",
+                 auto_save=True):
         self.agent = agent
-        self.episode = episode
+        self.episodes = episodes
+        self.auto_save = auto_save
+        self.save_path = save_path
+        self.save_episodes = save_episodes
+        self.target_update_episodes = target_update_episodes
+        self.model_name = model_name
 
-    def train(self, episodes=1000):
-        # each time of training will start the log
-        writer = SummaryWriter("./logs")
-        for i in range(episodes):
-            state = self.agent.env.reset()
-            done = False
-            total_reward = 0
-            while not done:
-                action = self.agent.act(state)
-                next_state, reward, done = self.agent.env.step(action)
-                total_reward += reward
-                self.agent.memory.push(state, action, reward, next_state, done)
-                state = next_state
+    def train(self):
+        # check whether need to reopen a new summary writer of just use the old one
+        if self.agent.pretrain_path:
+            # new to reuse the pretrained logs
+            start_episode = self.agent.pretrain_episode
+            # self.agent.pretrain_path (pretrained_models/05-13_MLP_50.pth)
+            pretrain_time = os.path.basename(self.agent.pretrain_path).split("_")[0]
+            writer = SummaryWriter(f"./logs/{pretrain_time}")
+            training_range = range(start_episode, self.episodes)
+        else:
+            # local time 5-13_12-45
+            local_time = time.strftime("%Y-%m-%d", time.localtime())
+            start_episode = 0
+            # open a new one
+            writer = SummaryWriter(f"./logs/{local_time}")
+            training_range = range(start_episode, self.episodes)
+        try:
+            for i in training_range:
+                state = self.agent.env.reset()
+                # state: numpy [H, W, 3]
+                done = False
+                total_reward = 0
+                while not done:
+                    tensor_state = torch.FloatTensor(state).unsqueeze(0).permute(0, 3, 1, 2)
+                    # tensor_state [1, H, W, 3]
+                    action = self.agent.act(tensor_state)
+                    next_state, reward, done = self.agent.env.step(action, self.agent.steer_dim)
+                    total_reward += reward
+                    # state, next_state: numpy [H, W, 3]
+                    self.agent.memory.push(state, action, reward, next_state, done)
+                    state = next_state
 
-                if len(self.agent.memory) > self.agent.batch_size:
-                    loss = self.agent.update_network(i)
-                    # write the log file
-                    writer.add_scalar("train_loss", loss.item(), i)
+                    if len(self.agent.memory) > self.agent.batch_size:
+                        loss = self.agent.update_network()
+                        # write the log file
+                        writer.add_scalar("train_loss", loss.item(), i)
 
-            # each 10 episode, target network will load the parameter from the q_network
-            if i % 10 == 0:
-                self.agent.target_network.load_state_dict(self.agent.q_network.state_dict())
+                # each 10 episode, target network will load the parameter from the q_network
+                if i % self.target_update_episodes == 0:
+                    self.agent.target_network.load_state_dict(self.agent.q_network.state_dict())
 
-            # save the model for every 100 episodes
-            if i % 100 == 0:
-                self.agent.save_model(f"models/model_{i}.pt")
+                # save the model for every 100 episodes
+                if i % self.save_episodes == 0 and self.auto_save and i != start_episode:
+                    save_time = time.strftime("%Y-%m-%d", time.localtime())
+                    self.save_model(os.path.join(self.save_path, f"{save_time}_{self.model_name}_{i}.pth"), i)
+                    print(f"save model in episodes {i}")
 
-            v = self.agent.env.vehicle.get_velocity()
-            kmh = int(3.6 * math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2))
-            print("Episode: {}, Total reward: {}, speed: {:.2f}".format(
-                i, total_reward, kmh)
-            )
-            # write the log file
-            writer.add_scalar("Total reward", total_reward, i)
-            writer.add_scalar("epsilon", self.agent.epsilon, i)
+                # print the training log
+                v = self.agent.env.vehicle.get_velocity()
+                kmh = int(3.6 * math.sqrt(v.x ** 2 + v.y ** 2 + v.z ** 2))
+                print("Episode: {}, Total reward: {:.2f}, epsilon: {:.2f}".format(
+                    i, total_reward, self.agent.epsilon)
+                )
 
-            # destory every vehicle in the actor_list when finishing each episodes
-            # print('destroying actors')
-            self.agent.env.client.apply_batch([carla.command.DestroyActor(x) for x in self.agent.env.actor_list])
-            for sensor in self.agent.env.sensor_list:
-                sensor.destroy()
-            # print('done')
-        writer.close()
+                # write the summary_writer file
+                writer.add_scalar("Total reward", total_reward, i)
+                writer.add_scalar("epsilon", self.agent.epsilon, i)
+                # destory every vehicle in the actor_list when finishing each episodes
+                self.agent.env.client.apply_batch([carla.command.DestroyActor(x) for x in self.agent.env.actor_list])
+                for sensor in self.agent.env.sensor_list:
+                    sensor.destroy()
 
-    def save_model(self, path):
-        torch.save(self.agent.q_network.state_dict(), path)
+            writer.close()
+        except KeyboardInterrupt:
+            print("--------------")
+            print("Exit")
+        finally:
+            if len(self.agent.env.actor_list) != 0:
+                # destory every vehicle in the actor_list when finishing each episodes
+                self.agent.env.client.apply_batch([carla.command.DestroyActor(x) for x in self.agent.env.actor_list])
+            if len(self.agent.env.sensor_list) != 0:
+                for sensor in self.agent.env.sensor_list:
+                    sensor.destroy()
+            print("destory all agent and sensor")
 
-    def load_model(self, path):
-        self.agent.q_network.load_state_dict(torch.load(path))
-        self.agent.target_network.load_state_dict(self.agent.q_network.state_dict())
+    def save_model(self, path, episode):
+        # only save the parameter of the model, not the whole network structure
+        torch.save({'episode': episode,
+                    'epsilon': self.agent.epsilon,
+                    'optimizer_dict': self.agent.optimizer.state_dict(),
+                    'model_dict': self.agent.q_network.state_dict()},
+                   path)
 
 
 def main():
-    # Create a DQNAgent and start training
-    state_dim = 3
-    action_dim = 3
+    # predefined parameters
     episodes = 1000
-    agent = DQNAgent(state_dim, action_dim)
-    trainer = AgentTrainer(agent, episodes)
+    save_episodes = 50
+    target_update_episodes = 10
+    show_cam = True
+    model_name = "MobileNet_v3"
+    batch_size = 8
+    im_width = 320
+    im_height = 240
+    throttle_list = [0.2, 0.4, 0.6, 0.8, 1]
+    steer_list = [0, 1, 2]
+    pretrain_path = None
+    # pretrain_path = 'pretrained_models/2023-05-14_MobileNet_v3_50.pth'
+
+    # Create a DQNAgent and start training
+    agent = DQNAgent(im_width, im_height, throttle_list=throttle_list, steer_list=steer_list, show_cam=show_cam,
+                     batch_size=batch_size, pretrain_path=pretrain_path)
+    trainer = AgentTrainer(
+        agent, episodes, save_episodes, target_update_episodes, model_name=model_name
+    )
+    print("start training !!")
     trainer.train()
 
 
